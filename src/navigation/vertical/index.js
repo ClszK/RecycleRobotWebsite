eea7b743821efc2c88692198ef40d8d17cf2d107
ljh <ljh@ljhui-MacBookAir.local>
2022-11-27 15:13:20 +0900

    First Commit

---
+++ src/navigation/vertical/index.js
@@ -0,0 +1,38 @@
+// ** Icon imports
+import Login from 'mdi-material-ui/Login'
+import HomeOutline from 'mdi-material-ui/HomeOutline'
+import AccountCogOutline from 'mdi-material-ui/AccountCogOutline'
+import AccountPlusOutline from 'mdi-material-ui/AccountPlusOutline'
+import AlertCircleOutline from 'mdi-material-ui/AlertCircleOutline'
+
+const navigation = () => {
+  return [
+    {
+      title: 'Dashboard',
+      icon: HomeOutline,
+      path: '/'
+    },
+    {
+      title: 'Account Settings',
+      icon: AccountCogOutline,
+      path: '/account-settings'
+    },
+    {
+      sectionTitle: 'Pages'
+    },
+    {
+      title: 'Login',
+      icon: Login,
+      path: '/pages/login',
+      openInNewTab: true
+    },
+    {
+      title: 'Register',
+      icon: AccountPlusOutline,
+      path: '/pages/register',
+      openInNewTab: true
+    }
+  ]
+}
+
+export default navigation
